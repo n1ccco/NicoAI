3173fe64ca1b6e739e656741d50e3155a9656c49
Bohdan Zhuvak <zhuvak.bohdan@gmail.com>
2024-04-13 19:37:04 +0200

    Init Commit

---
+++ backend/src/main/java/org/bohdanzhuvak/nicoai/NicoAiApplication.java
@@ -0,0 +1,13 @@
+package org.bohdanzhuvak.nicoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicoAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NicoAiApplication.class, args);
+    }
+
+}
